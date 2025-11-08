5c6b14f3abff8cf427a850d8607d4d64719150d5
sophie yang <121143211+glossysauce@users.noreply.github.com>
2025-11-08 03:02:25 -0500

    init

---
+++ Core/Src/main.c
@@ -0,0 +1,387 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <math.h>
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
+
+/* USER CODE BEGIN PV */
+
+static volatile int upCount = 0;
+static volatile int downCount = 0;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+
+	void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+
+		//button debouncing :p
+		static uint32_t lastTick = 0;
+		uint32_t currentTick = HAL_GetTick();
+		//ignore if less than 50ms
+		if(currentTick - lastTick < 50){
+			return;
+		}
+		lastTick = HAL_GetTick();
+
+		//D4
+		if (GPIO_Pin == GPIO_PIN_5){
+			upCount++;
+		}
+		//D2
+		else if ((GPIO_Pin == GPIO_PIN_10)){
+			upCount--;
+		}
+
+	}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+	static const float frequencies[] = {
+			659.26, 493.88, 523.25, 587.33, 523.25, 493.88, 440.00, 220.00, 440.00, 523.25, 659.26, 587.33, 523.25, 493.88, 415.30, 523.25, 587.33, 659.26, 523.25, 440.00, 220.00, 220.00, 587.33, 698.46, 880.00, 783.99, 698.46, 659.26, 523.25, 659.26, 587.33, 523.25, 493.88, 246.94, 261.63, 587.33, 659.26, 523.25, 440.00
+	};
+	//ms
+	static const float durations[] = {
+			406.35, 191.56, 185.76, 365.71, 203.17, 191.56, 162.54, 162.54, 168.34, 168.34, 371.52, 203.17, 191.56, 388.93, 145.12, 203.17, 377.32, 388.93, 383.13, 191.56, 609.52, 87.07, 406.35, 185.76, 383.13, 191.56, 191.56, 522.45, 214.78, 377.32, 197.37, 185.76, 214.78, 319.27, 168.34, 342.49, 394.74, 383.13, 853.33
+	};
+	//calculate # of notes
+	int numNotes = sizeof(frequencies) / sizeof(frequencies[0]);
+
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
+
+//  uint32_t * TIM2BASE = (uint32_t *) 0x40000000;
+
+
+
+  //volume is the ccr duty cycle. CCR = duty * ARR+1
+  //ask user for volume from 0-100
+  float duty = 0.0;
+
+
+
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  for (int i = 0; i < numNotes; i++){
+
+		  //base freq
+		  int fifth = upCount - downCount;
+
+		  //calculate for major freq shift
+		  float mult = powf(2.0, (1.0/12.0) * fifth);
+
+		  //change note to adjusted frequency
+		  float note = frequencies[i] * mult;
+
+
+		  //Frequency is the ARR period, ARR = tick/freq - 1
+		  uint32_t arr = (16000000 / note) - 1;
+		  __HAL_TIM_SET_AUTORELOAD(&htim2, arr);
+
+		  //set volume
+		  //volume is proportional to sin(pi * DC), max vol at 50% dc
+		  float volume = duty/200.0 * (arr + 1);
+		  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, volume);
+
+		  //play
+		  __HAL_TIM_SET_COUNTER(&htim2, 0);
+		  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
+		  HAL_Delay(durations[i]);
+		  HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_3);
+
+	  }
+
+	  ///debug
+//	  uint32_t arr = (16000000 / frequencies[0]) - 1;
+//	  		  __HAL_TIM_SET_AUTORELOAD(&htim2, arr);          // 1 kHz @ 16 MHz tick
+//
+//	  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, 0); // ~50% duty
+//	  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 4;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 4294967295;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : USART_TX_Pin USART_RX_Pin */
+  GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ FFT/FFTNoteExtracter.py
@@ -0,0 +1,97 @@
+# SOPHIE YANG - FFT NOTE EXTRACTER
+import numpy as np
+import librosa
+import scipy.signal as sig
+
+#butterworth highpass with cutoff at 180, order of 4
+#user bwf for smooth analog rolloff
+def butter_bandpass(x, sr, low=250.0, high=3500.0, order=4):
+    b, a = sig.butter(order, [low/(0.5*sr), high/(0.5*sr)], btype='bandpass')
+    return sig.filtfilt(b, a, x)
+
+def extract_notes(wav_file):
+    # loading the wav file
+    y, sr = librosa.load(wav_file, sr=None)
+    
+    # isolate clean tones with harmonic-percussive separation
+    y_harmonic, _ = librosa.effects.hpss(y)
+
+    #apply bwf
+    y_hp = butter_bandpass(y_harmonic, sr, low=250.0, high=1000.0, order=4)
+
+    # perform short term fourier transform
+    n_fft = 4096  # increased for better frequency accuracy, this is # of points for FFT
+    hop_length = 512 #step size between FFT
+    stft = librosa.stft(y_hp, n_fft=n_fft, hop_length=hop_length) #computes the short term fourier transform
+
+    # get the magnitude spectrogram
+    # since stft contains real and imag parts, we need to extract the magnitude + ignore phase
+    magnitude = np.abs(stft) 
+
+    # convert magnitude to dB scale, this is how humans perceive loudness
+    # this will prevent small peaks from being overshadowed by loud freqs!
+    log_magnitude = librosa.amplitude_to_db(magnitude)
+
+    # find peaks in the spectrogram to detect notes
+    frequencies = librosa.fft_frequencies(sr=sr, n_fft=n_fft)
+    times = librosa.frames_to_time(np.arange(log_magnitude.shape[1]), sr=sr, hop_length=hop_length)
+
+    # detect notes by finding peaks in the frequency domain
+    # note = highest peak in each frame
+    notes = []
+    for i in range(log_magnitude.shape[1]):
+        frame = log_magnitude[:, i]
+        peaks, _ = sig.find_peaks(frame, height=np.mean(frame) - 10)
+        if len(peaks) > 0:
+            dominant_freq = frequencies[peaks[np.argmax(frame[peaks])]]
+            notes.append((dominant_freq, times[i]))
+
+    #grouping similar freqs into notes
+    note_durations = []
+    if not notes:
+        return note_durations  # empty if no notes were detected
+
+    prev_freq = notes[0][0]  # first detected frequency
+    start_time = notes[0][1]  # first note start time
+    freq_tolerance = 2  # 2hz tolerance
+    dur_tolerance = 80 #reject any note duration shorter than dur_tolerance ms, can be changed based on bpm
+
+# group consecutive and similar frequencies into a single note
+    for i in range(1, len(notes)):
+        freq, timestamp = notes[i]
+        
+        if abs(freq - prev_freq) > freq_tolerance:  # detect significant changes
+            duration = (timestamp - start_time) * 1000  # convert to ms
+            
+            if duration > dur_tolerance:  # apply duration filter
+                note_durations.append((prev_freq, duration))
+            
+            start_time = timestamp  # reset start time
+            prev_freq = freq  # update frequency
+
+    # handle the last note
+    duration = (times[-1] - start_time) * 1000
+    if duration > dur_tolerance:
+        note_durations.append((prev_freq, duration))
+
+    return note_durations
+
+#export notes and durations in a txt file
+def export_notes(note_durations, output_file):
+    with open(output_file, 'w') as f:
+        frequencies = [f"{freq:.2f}" for freq, _ in note_durations]
+        f.write(", ".join(frequencies))
+
+def export_durations(note_durations, output_file):
+    with open(output_file, 'w') as f:
+        durations = [f"{duration:.2f}" for _, duration in note_durations]
+        f.write(", ".join(durations))
+
+# import audio as wav
+wav_file = "tetris.wav"
+note_durations = extract_notes(wav_file)
+
+export_notes(note_durations, "notes.txt")
+export_durations(note_durations, "durations.txt")
+
+print("notes + durations extracted and saved")
